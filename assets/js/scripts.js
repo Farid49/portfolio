7d515244e5ede3bce5e58434ea669b45fa0c4daa
Farid49 <faridhoveida@outlook.com>
2024-01-21 18:10:50 +0330

    fix all files

--- assets/js/scripts.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("moreinfo").addEventListener("click" , ()=>{
-    document.querySelector("input.toggler").checked = true;
-})
--- assets/js/youtube-bg.js
+++
@@ -1,1 +0,0 @@
-const videoBackgrounds = new VideoBackgrounds('[data-vbg]');
